11d66987de7340fda307dc76d5e0cbbbeff4d209
Ajay-Dhangar <ajaydhangar49@gmail.com>
2024-09-05 11:41:15 +0530

    update codde

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -83,7 +83,7 @@ const config: Config = {
         },
         { to: "/blog", label: "Blog", position: "left" },
         {
-          href: "https://github.com/facebook/docusaurus",
+          href: "https://github.com/eco-farm-hub",
           label: "GitHub",
           position: "right",
         },
@@ -106,15 +106,15 @@ const config: Config = {
           items: [
             {
               label: "Stack Overflow",
-              href: "https://stackoverflow.com/questions/tagged/docusaurus",
+              href: "#",
             },
             {
               label: "Discord",
-              href: "https://discordapp.com/invite/docusaurus",
+              href: "#",
             },
             {
               label: "Twitter",
-              href: "https://twitter.com/docusaurus",
+              href: "#",
             },
           ],
         },
@@ -127,7 +127,7 @@ const config: Config = {
             },
             {
               label: "GitHub",
-              href: "https://github.com/facebook/docusaurus",
+              href: "https://github.com/eco-farm-hub",
             },
           ],
         },
